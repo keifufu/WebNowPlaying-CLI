228ecb7554147c2ceccc93c08e62835385aab783
keifufu <github@keifufu.dev>
2024-10-06 21:55:20 +0200

    libwnp

--- build.ps1
+++ build.ps1
@@ -14,22 +14,40 @@ function Invoke-Environment {
 }
 
 $origEnv = Get-ChildItem Env:*;
+$vsInstallationPath = & 'vswhere.exe' -property installationPath
 
-Remove-Item -Path "$PSScriptRoot\build" -Recurse -Force
-New-Item -Path "$PSScriptRoot\build" -ItemType Directory
-New-Item -Path "$PSScriptRoot\build\x64" -ItemType Directory
-Remove-Item -Path "$PSScriptRoot\dist" -Recurse -Force
-New-Item -Path "$PSScriptRoot\dist" -ItemType Directory
-New-Item -Path "$PSScriptRoot\dist\x64" -ItemType Directory
-
-Invoke-Environment "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-Set-Location "$PSScriptRoot\build\x64"
-cmake -G "NMake Makefiles" -Dlibwnp_DIR="C:\Users\keifufu\Desktop\WebNowPlaying-Library\dist\x64\lib\cmake\libwnp" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\dist\x64" "$PSScriptRoot"
-cmake --build .
-cmake --install .
-
-$origEnv | ForEach-Object {
-  [System.Environment]::SetEnvironmentVariable($_.Name, $_.Value)
+if (-not $vsInstallationPath) {
+  Write-Host "No Visual Studio installation found."
+  exit 1
 }
 
-Set-Location "$PSScriptRoot"
+if (-Not (Test-Path "$PSScriptRoot/libwnp")) {
+  git clone https://github.com/keifufu/WebNowPlaying-Library "$PSScriptRoot/libwnp"
+  & "$PSScriptRoot\libwnp\build.ps1"
+}
+$LIBWNP_DIR = "$PSScriptRoot\libwnp\build\win64\lib\cmake\libwnp"
+
+Remove-Item -Path "$PSScriptRoot\build" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$PSScriptRoot\dist" -Recurse -Force -ErrorAction SilentlyContinue
+
+New-Item -Path "$PSScriptRoot\build" -ItemType Directory
+New-Item -Path "$PSScriptRoot\dist" -ItemType Directory
+
+Invoke-Environment "$vsInstallationPath\VC\Auxiliary\Build\vcvars64.bat"
+cmake -S "$PSScriptRoot" -B "$PSScriptRoot\build" -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\build" -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -Dlibwnp_DIR="$LIBWNP_DIR"
+cmake --build "$PSScriptRoot\build"
+cmake --install "$PSScriptRoot\build"
+cpack -B "$PSScriptRoot\dist" --config "$PSScriptRoot\build\CPackConfig.cmake"
+Remove-Item -Path "$PSScriptRoot\dist\_CPack_Packages" -Recurse -Force -ErrorAction SilentlyContinue
+$origEnv | ForEach-Object { [System.Environment]::SetEnvironmentVariable($_.Name, $_.Value) }
+
+$files = @(
+  "$PSScriptRoot\README.md",
+  "$PSScriptRoot\LICENSE",
+  "$PSScriptRoot\CHANGELOG.md",
+  "$PSScriptRoot\VERSION",
+  "$PSScriptRoot\build\bin"
+)
+  
+$version = (Get-Content "$PSScriptRoot\VERSION" | Out-String).Trim()
+Compress-Archive -Path $files -DestinationPath "$PSScriptRoot\dist\wnpcli-${version}_win64.zip" -CompressionLevel Optimal
